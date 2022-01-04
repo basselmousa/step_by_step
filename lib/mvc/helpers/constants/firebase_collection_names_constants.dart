5f0eabc376981b32fd79cd23afbd67314e609dd1
bassel-mousa <--get>
2022-01-04 02:03:40 +0200

    Make Fire base collection constants

--- lib/mvc/helpers/constants/firebase_collection_names_constants.dart
+++ lib/mvc/helpers/constants/firebase_collection_names_constants.dart
@@ -1,5 +1,5 @@
 class FirebaseCollectionNamesConstants{
   static const USER_COLLECTION_NAME = 'users';
-  static const PRODUCTS_COLLECTION_NAME = 'products';
-  static const CATEGORY_COLLECTION_NAME = 'categories';
+  static const ROLES_COLLECTION_NAME = 'roles';
+  static const RESERVATIONS_COLLECTION_NAME = 'reservations';
 }
