20614092a09d3f887d63e2ee0a1bbee321c6af33
bassel-mousa <--get>
2022-01-04 02:03:11 +0200

    Making Add book function

--- lib/mvc/controllers/admins/admin_controller.dart
+++ lib/mvc/controllers/admins/admin_controller.dart
@@ -1,6 +1,13 @@
+
+
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:step_by_step/mvc/models/book_model.dart';
+import 'package:step_by_step/mvc/utils/utils.dart';
 
 class AdminController {
   static Future<void> addOrUpdateRole({
@@ -68,5 +75,18 @@ class AdminController {
         'current' : value.data()['current'] != null ? value.data()['current']+  1 : 2
       });
     });
+  }
+
+
+  Future<void> addBook(Book book, File image, BuildContext context)async{
+    book.imagePath = await uploadImage(image);
+    await FirebaseFirestore.instance.collection('books').doc().set(book.toJSON()).then((value) => buildSnackBar(context, "Book Added"));
+
+  }
+  static Future<String> uploadImage(File image) async{
+    String path = image.toString().substring(image.toString().indexOf('P'));
+    var fileName = await FirebaseStorage.instance.ref(path).putFile(image);
+
+    return await fileName.ref.getDownloadURL();
   }
 }
